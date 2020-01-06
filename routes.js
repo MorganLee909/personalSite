74f52460c773ded99588bc197b388b90baf47493
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-01-07 00:18:21 +0200

    Finish sudoku style

--- routes.js
+++ routes.js
@@ -3,9 +3,8 @@ const home = require("./controllers/home.js");
 module.exports = function(app){
     app.get("/", home.renderMain);
     app.get("/jeopardy", home.renderJeopardy);
+    app.get("/sudoku", home.sudoku);
 
     app.post("/jeopardy/question/create", home.createQuestion);
     app.post("/jeopardy/questions", home.getQuestions);
-    
-    app.get("/sudoku", home.sudoku);
 }
