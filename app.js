007a1b740dcde04a81f32f8cf5b33ab17bee5a75
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-09-13 18:22:40 +0300

    Add compression.  Update package.json.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
 const app = express();
 
@@ -10,9 +11,8 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/views"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(compression());
 
 require("./routes")(app);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT, ()=>{});
