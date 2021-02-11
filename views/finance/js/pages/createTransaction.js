34367a0bf1e37dd1dbab0e02a0132cb874198613
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-02-11 12:05:55 +0300

    Add optgroups to transaction dropdown for clarity.

--- views/finance/js/pages/createTransaction.js
+++ views/finance/js/pages/createTransaction.js
@@ -9,25 +9,44 @@ let createTransaction = {
                 selector.removeChild(selector.firstChild);
             }
 
+            let bills = document.createElement("optgroup");
+            bills.label = "BILLS";
+            selector.appendChild(bills);
             for(let i = 0; i < state.user.account.bills.length; i++){
                 let option = document.createElement("option");
                 option.innerText = state.user.account.bills[i].name;
                 option.value = state.user.account.bills[i].name;
-                selector.appendChild(option);
+                bills.appendChild(option);
             }
 
+            let income = document.createElement("optgroup");
+            income.label = "INCOME";
+            selector.appendChild(income);
             for(let i = 0; i < state.user.account.income.length; i++){
                 let option = document.createElement("option");
                 option.innerText = state.user.account.income[i].name;
-                option.value = state.user.account.income[i].name;
-                selector.appendChild(option);
+                option.value = state.user.account.income[i].sname;
+                income.appendChild(option);
             }
 
+            let allowances = document.createElement("optgroup");
+            allowances.label = "ALLOWANCES";
+            selector.appendChild(allowances);
+            for(let i = 0; i < state.user.account.allowances.length; i++){
+                let option = document.createElement("option");
+                option.innerText = state.user.account.allowances[i].name;
+                option.value = state.user.account.allowances[i].name;
+                allowances.appendChild(option);
+            }
+
+            let other = document.createElement("optgroup");
+            other.label = "OTHER";
+            selector.appendChild(other);
             for(let i = 0; i < state.user.account.categories.length; i++){
                 let option = document.createElement("option");
                 option.innerText = state.user.account.categories[i];
                 option.value = state.user.account.categories[i];
-                selector.appendChild(option);
+                other.appendChild(option);
             }
 
             state.createTransactionPage.newCategories === false;
